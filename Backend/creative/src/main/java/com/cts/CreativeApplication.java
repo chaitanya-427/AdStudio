cb0dc0cc1105b1ce1b162feaf21b7e7044793d85
Sailesh-9 <Guntamukkala.Sailesh@cognizant.com>
2026-06-11 12:25:28 +0530

    Completed creative module

--- Backend/creative/src/main/java/com/cts/CreativeApplication.java
+++ Backend/creative/src/main/java/com/cts/CreativeApplication.java
@@ -1,4 +1,4 @@
-package com.cts.creative;
+package com.cts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class CreativeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreativeApplication.class, args);
+		System.out.println("Springboot running.......");
 	}
 
 }
